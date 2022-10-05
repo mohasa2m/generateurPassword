5e4643286205b65bd726e3815f82152421244f7d
mohamed sammar <mohamed.smr16@gmail.com>
2022-10-05 20:16:07 +0200

    generateur de mot de passe

---
+++ generateur_de_mot_de_passe/essaye_de_moha/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace essaye_de_moha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NB_MOT_DE_PASSE = 10;
+            int longueurMotDePasse = outils.DemanderNombrePositifNonNul("Longueur du mot de passe: ");
+
+            Console.WriteLine();
+
+            int choixAlphabet = outils.DemanderNombreEntre("Vous voulez un mot de passe avec:\n" +
+                "1- Uniquement avec des caractères en minuscules\n" +
+                "2- Des caractères en majuscules et minuscules\n" +
+                "3- Des caractères et des chiffres\n" +
+                "4- Caractères, chiffres et caractères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+            string minuscule = "abcdefghijklmnopqrstuvwxyz";
+            string majuscule = minuscule.ToUpper();
+            string chiffres = "123456789";
+            string caracteresSpeciaux = "#&+-";
+            string alphabet;
+            string motDePasse = "";
+            Random rand = new Random();
+
+
+
+            if (choixAlphabet == 1)
+                alphabet = minuscule;
+            else if (choixAlphabet == 2)
+                alphabet = minuscule + majuscule;
+            else if (choixAlphabet == 3)
+                alphabet = minuscule + majuscule + chiffres;
+            else
+                alphabet = minuscule + majuscule + chiffres + caracteresSpeciaux;
+
+            int longueurAlphabet = alphabet.Length;
+
+            for (int j= 0; j < NB_MOT_DE_PASSE; j++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longueurAlphabet);
+                    motDePasse += alphabet[index];
+                }
+
+                Console.WriteLine("Mot de passe: " + motDePasse);
+            }
+            
+
+  
+        }
+    }
+}
---
+++ generateur_de_mot_de_passe/essaye_de_moha/outils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace essaye_de_moha
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue,"ERREUR: le nombre doit être positif et non nul");
+        }
+        public static int DemanderNombreEntre(string question, int min, int max, string messageErreurPersonnalise = null)
+        {
+
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                return nombre;
+            }
+            if(messageErreurPersonnalise == null)
+            {
+                Console.WriteLine("Erreur le nombre doit être compris entre " + min + " et " + max);
+
+            }
+            else
+            {
+                Console.WriteLine(messageErreurPersonnalise);
+            }
+            Console.WriteLine();
+
+            return DemanderNombreEntre(question, min, max, messageErreurPersonnalise);
+        }
+
+        public static int DemanderNombre(string question)
+        {
+
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR: Vous devez rentrer un nombre");
+                    Console.WriteLine();
+                }
+
+            }
+
+        }
+    }
+}
